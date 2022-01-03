1f225bfeee87da70f4090cd0cfae34675968d44c
oiza-web <maryolorunfemi6@gmail.com>
2022-01-03 12:30:13 +0100

    added bootstrap

---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+import { createWebHistory, createRouter } from "vue-router";
+import Home from "@/views/Home.vue";
+import SearchResults from "@/views/SearchResults.vue";
+import PhotoDetails from "@/views/PhotoDetails.vue";
+import NotFound from "@/views/NotFound.vue";
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/search/:tag",
+    name: "searchResults",
+    component: SearchResults,
+    props: true,
+  },
+  {
+    path: "/photo/:id/:secret", // view one picture
+    name: "photoDetails",
+    component: PhotoDetails,
+    props: true,
+  },
+
+  //Not found
+  {
+    path: "/:catchAll(.*)",
+    component: NotFound,
+  },
+];
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
